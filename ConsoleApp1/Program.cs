427d080c210ca1e1295fa070bd56717cb490abb1
Хабибулин Даниил Олегович <khabibulin.do1648@okeit.edu>
2022-10-06 09:31:12 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*string text = Console.ReadLine();
+            char [] arr = new char[text.Length];
+            for (int i = 0; i < text.Length; i ++ )
+            {
+                arr[i] = text[i];
+            }
+            for (int j = 0; j < text.Length; j += 2)
+            {
+                arr[j] = Char.ToUpper(arr[j]);
+            }
+            Console.WriteLine(arr);*/
+
+            //Console.Write("напишите число:");
+            // try
+            // {
+            //     int num = int.Parse(Console.ReadLine());
+            //     if (num > 1000)
+            //     {
+            //         num = 0;
+            //         Console.WriteLine("А че так много");
+            //     }
+            //
+            //     for (int i = 0; i < num + 1; i++)
+            //     {
+            //         Console.WriteLine(i);
+            //     }
+            // }
+            // catch (System.FormatException)
+            // {
+            //     Console.WriteLine("Вы ввели не число");
+            // }
+
+           // string text = Console.ReadLine();
+           // if(text.Length == 5)
+           // {
+           //     string n_text = "";
+           //     for (int i = 0; i < text.Length; i++)
+           //     {
+           //         n_text += text[text.Length - i - 1];
+           //     }
+           //     Console.WriteLine(n_text);
+           // }
+           // else 
+           // {
+           //     Console.WriteLine("В массиве не 5 элементов");
+           // }
+        }  
+    }
+}
